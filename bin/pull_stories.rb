dcdf21fa750162311decc5c6d108f905937c03ca
Patrick Farrell <mr.patfarrell@gmail.com>
2016-01-28 22:07:30 -0800

    simple connections working

--- bin/pull_stories.rb
+++ bin/pull_stories.rb
@@ -1,30 +1,38 @@
 require 'tracker_api'
 require 'byebug'
 
-
-class Project
-  attr_accessor :client, :stories
-
+module Tracker
   def initialize
     @client=TrackerApi::Client.new(token: ENV['PIVOTAL_TOKEN'])
   end
+end
+
+class Project
+  include Tracker
+  attr_accessor :client, :stories
 
   def stories(opts={})
-    @stories ||= client.projects.first.stories.select{|s| s.labels.select{|l| l.name.downcase == 'nes' || l.name.downcase == 'enes'}.size > 0}
+    @stories ||= client.projects.first.stories.select{|s| (['nes','enes'] & s.labels.map(&:name)).size > 0}
     opts[:epic].nil? ? @stories : @stories.select{|s| s.labels.select{|l| l.name.downcase == "#{opts[:epic].downcase}"}.size > 0}
     #@stories
   end
 
   def active(opts={})
-    stories(opts).reject{|s| s.current_state == 'delivered' || s.current_state == 'unscheduled'}
+    stories(opts).reject{|s| ([s.current_state] & ['accepted','unscheduled']).size > 0}
   end
 end
 
+class Owner
+  include Tracker
+  attr_accessor :client, :owners
+
+end
 
 p = Project.new
 p p.stories.size
 p p.stories(epic: 'nes').size
 p p.stories(epic: 'enes').size
+p p.active(epic: 'enes').size
 byebug;1
 
 
